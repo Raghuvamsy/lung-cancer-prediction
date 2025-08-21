b8d558ff11fa15606da93c5a9932608ccb605fa8
Omm Prakash Jena <ojena643@gmail.com>
2025-08-21 11:07:01 +0530

    ,

---
+++ liver_canser.py
@@ -0,0 +1,9 @@
+#Importing Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#For ignoring warning
+import warnings
+warnings.filterwarnings("ignore")
